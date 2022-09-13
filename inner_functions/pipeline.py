8823b55f1899e1c061d5468bb8607b56eeb0972b
Adrian-Montemayor <adrianmoncar@hotmail.com>
2022-09-12 19:15:47 -0500

    add: inner function example

---
+++ inner_functions/pipeline.py
@@ -0,0 +1,10 @@
+def pipeline(*funcs):
+    def helper(arg):
+        res = arg
+        for func in funcs:
+            res = func(res)
+        return res
+    return helper
+
+fun = pipeline(lambda x: x*3, lambda x: x+1, lambda x:x/2)
+print(fun(3))
